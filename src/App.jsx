3e2812d258a497521054f7c33243fae3021afc2d
dayvancowboy13 <chris.e.friedrich@gmail.com>
2024-11-13 09:40:16 -0800

    Modify styling
    Removed unneccessary media queries and css declarations

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,17 @@ import './App.css'
 
 function App() {
 
+  // fetch('https://fakestoreapi.com/products/1')
+  //   .then(res => res.json())
+  //   .then(json => console.log(json))
+
   return (
     <>
       Work in progress :)
+      <a href='https://www.google.com'>Clicky</a>
+      <button>Test</button>
+      <p className="roboto">Roboto font</p>
+      <p className="inter">Inter font</p>
     </>
   )
 }
